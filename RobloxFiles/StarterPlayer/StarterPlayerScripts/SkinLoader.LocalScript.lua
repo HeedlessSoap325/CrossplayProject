16af1a00569cf79b998078e9fedff8a6e15690c5
M. Fritz <mathisjulian123@gmail.com>
2026-06-14 18:03:59 +0200

    feat: remove dependency on crossplayproject api Fetch the skin ul directly from the Minecraft Session-Server. issue: There is still a bug here, where the skin doesn't get applied correctly to the player model. Havn't figured that out yet

--- RobloxFiles/StarterPlayer/StarterPlayerScripts/SkinLoader.LocalScript.lua
+++ RobloxFiles/StarterPlayer/StarterPlayerScripts/SkinLoader.LocalScript.lua
@@ -4,21 +4,21 @@ local remote_img = require(game.ReplicatedStorage.remote_img)
 
 ReplicatedStorage:WaitForChild("loadPlayerSkin").OnClientEvent:Connect(function(uuid, player, Character)
 
-	local skin = remote_img.create_image("https://crossplayproject.xyz/api/uuid/"..uuid.."/skin")
+	local sessionUrl = "https://" .. game.ReplicatedStorage.SESSION_SERVER.Value .. "/session/minecraft/profile/" .. uuid
+	local skinUrl = remote_img.retreive_image_url(sessionUrl)
+	local skin = remote_img.create_image(skinUrl)
 	task.wait()
+
+	local skinContent = Content.fromObject(skin)
 	for _, part in pairs(Character.SecondLayer:GetChildren()) do
 
 		if part:IsA("MeshPart") then
-
-			local imageClone = skin:Clone()
-			imageClone.Parent = part
+			part.TextureContent = skinContent
 		end
 	end
 	for _, part in pairs(Character:GetChildren()) do
 		if part:IsA("MeshPart") then
-
-			local imageClone = skin:Clone()
-			imageClone.Parent = part
+			part.TextureContent = skinContent
 		end
 	end
 end)
